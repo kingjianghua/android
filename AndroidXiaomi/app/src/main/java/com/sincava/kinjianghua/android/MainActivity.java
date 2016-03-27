d2242d2e47c8ea87f550558075e2c799cabc815d
kingjianghua <e190540@yahoo.co.jp>
2016-03-27 18:34:31 +0900

    add onclick method

--- AndroidXiaomi/app/src/main/java/com/sincava/kinjianghua/android/MainActivity.java
+++ AndroidXiaomi/app/src/main/java/com/sincava/kinjianghua/android/MainActivity.java
@@ -1,7 +1,10 @@
 package com.sincava.kinjianghua.android;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+    public void count(View v)
+    {
+        EditText e1 =(EditText)findViewById(R.id.editText);
+        EditText e2 =(EditText)findViewById(R.id.editText2);
+        TextView result = (TextView)findViewById(R.id.textView2);
+
+        int num1 =Integer.parseInt(e1.getText().toString()) ;
+        int num2 =Integer.parseInt(e2.getText().toString());
+        int res = num1+num2;
+        result.setText(Integer.toString(res));
+
+    }
+
 }
